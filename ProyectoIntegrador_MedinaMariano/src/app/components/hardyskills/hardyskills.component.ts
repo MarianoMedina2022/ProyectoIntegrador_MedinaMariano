96dd1f4181880ae95f4656ebd415d6cb46ecf0f3
Mariano Medina <medinamariano1601@gmail.com>
2022-04-13 21:45:04 -0300

    Correcciones y cambios
    Avance hasta las Hard and soft skills e hice correcciones

---
+++ ProyectoIntegrador_MedinaMariano/src/app/components/hardyskills/hardyskills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hardyskills',
+  templateUrl: './hardyskills.component.html',
+  styleUrls: ['./hardyskills.component.css']
+})
+export class HardyskillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
