080d96a0bebe473b6cf2e343c36d88c4ba7d74f7
Mariano Medina <medinamariano1601@gmail.com>
2022-06-06 01:12:02 -0300

    nuevos cambios
    avance del proyecto

--- ProyectoIntegrador_MedinaMariano/FrontEnd/src/app/app.component.ts
+++ ProyectoIntegrador_MedinaMariano/FrontEnd/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { PersonaService } from './service/persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  viewProviders: [PersonaService]
 })
 export class AppComponent {
   title = 'ProyectoIntegrador_MedinaMariano';
+    
 }
